568e1c6be2293008f978348c26fb03fcfe12e3f5
Mashylyamur <krabovna3@gmail.com>
2023-03-15 07:12:11 +0300

    some commit

--- module5_21.py
+++ module5_21.py
@@ -1,18 +1,17 @@
 import json
 data = {}
-with open('model.json', 'w') as f:
-    json.dump(data, f)
 class Model:
     def __init__(self):
         self.title = '1'
         self.text = '2'
         self.author = '3'
-    def save(self):
-        with open('model.json', 'r') as f:
-            data = json.load(f)
-            Model.__dict__
+    def save(self, data):
+        with open('model.json', 'w') as f:
+            json.dump(data, f)
+            data = self.__dict__
         with open('model.json', 'w') as f:
             json.dump(data, f)
 s = Model()
-s.save()
+s.save(data)
+
 
